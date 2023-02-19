ffb3ec0d6ee3baa4820d0fa1de719471971e632a
Lukasz Wisniewski <lukasz.wisniewski48@wp.pl>
2023-02-19 04:15:51 +0100

    build: created custom input  component

---
+++ src/Components/UI/InputField/InputField.tsx
@@ -0,0 +1,38 @@
+interface Props {
+  title: string;
+  type: string;
+  placeholder?: string;
+  error?: string;
+  value: string;
+  setNevValue: (value: string) => void;
+}
+
+export function InputField({
+  placeholder,
+  title,
+  type,
+  error,
+  value,
+  setNevValue,
+}: Props) {
+  const changeValueHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.value.length > 40) {
+      return;
+    }
+    setNevValue(event.target.value);
+  };
+
+  return (
+    <span>
+      <label htmlFor={title}>{title}</label>
+      <input
+        type={type}
+        id={title}
+        placeholder={placeholder}
+        value={value}
+        onChange={(event) => changeValueHandler(event)}
+      ></input>
+      {error && <label style={{ color: "red", fontSize: 12 }}>{error}</label>}
+    </span>
+  );
+}
