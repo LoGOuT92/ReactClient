2666e56218841267324d6865c8e79c3b9fe01b61
Lukasz Wisniewski <lukasz.wisniewski48@wp.pl>
2023-02-20 00:45:47 +0100

    fix: fixed validate functions

--- src/Components/ItemsList/ListAddItem/ListAddItem.tsx
+++ src/Components/ItemsList/ListAddItem/ListAddItem.tsx
@@ -4,26 +4,40 @@ import { NumericInput } from "../../UI/NumericInput/NumericInput";
 import styles from "./ListAddItem.module.scss";
 import { InputField } from "../../UI/InputField/InputField";
 import { numberValidate } from "../../UI/numberValueValidate";
+import { textValueValidate } from "../../UI/textValueValidate";
 
 export function ListAddItem({
   addItemVisibility,
+  createNewItemHandler,
 }: {
   addItemVisibility: boolean;
+  createNewItemHandler: (name: string, value: number) => Promise<void>;
 }) {
   const [value, setValue] = useState<number>(0);
   const [channelName, setChannelName] = useState<string>("");
+
   const [channelError, setchannelError] = useState("");
   const [valueError, setvalueError] = useState("");
 
   const AddNewItemHandler = () => {
     setvalueError(numberValidate(value));
-    channelName.length === 0
-      ? setchannelError("Channel name cannot be empty!")
-      : setchannelError("");
+    setchannelError(textValueValidate(channelName));
+
+    if (!textValueValidate(channelName) && !numberValidate(value)) {
+      createNewItemHandler(channelName, value);
+      setValue(0);
+      setChannelName("");
+    }
   };
 
   return (
-    <li className={`${!addItemVisibility ? styles.ListAddItemContainer : ""}`}>
+    <li
+      className={`${
+        !addItemVisibility
+          ? styles.ListAddItemContainer
+          : styles.ListAddItemContainer2
+      }`}
+    >
       <InputField
         title="Kanał"
         type="text"
