5c1a2659a9d8fbaa9bddad7157499eea383df8cc
Lukasz Wisniewski <lukasz.wisniewski48@wp.pl>
2023-02-21 02:12:07 +0100

    feat: added types

--- src/Components/UI/colorGenerator.ts
+++ src/Components/UI/colorGenerator.ts
@@ -1,18 +1,19 @@
-function randomInteger(max:number) {
-    return Math.floor(Math.random()*(max + 1));
-}
-
-function randomRgbColor() {
-    let r = randomInteger(255);
-    let g = randomInteger(255);
-    let b = randomInteger(255);
-    return [r,g,b];
-}
-
-export function randomHexColor() {
-    let [r,g,b] =randomRgbColor();
-    let hr = r.toString(16).padStart(2, '0');
-    let hg = g.toString(16).padStart(2, '0');
-    let hb = b.toString(16).padStart(2, '0');
-    return "#" + hr + hg + hb;
-}
+//generates random int form 0 to max param
+function randomInteger(max: number): number {
+    return Math.floor(Math.random() * (max + 1));
+  }
+//generates random RGB
+  function randomRgbColor(): [number, number, number] {
+    const r = randomInteger(255);
+    const g = randomInteger(255);
+    const b = randomInteger(255);
+    return [r, g, b];
+  }
+//generates random HEX color string
+  export function randomHexColor(): string {
+    const [r, g, b] = randomRgbColor();
+    const hr = r.toString(16).padStart(2, "0");
+    const hg = g.toString(16).padStart(2, "0");
+    const hb = b.toString(16).padStart(2, "0");
+    return `#${hr}${hg}${hb}`;
+  }
