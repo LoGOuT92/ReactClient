62ec9d42dd2db6bfe19e3199f94bc7be66910b8b
Lukasz Wisniewski <lukasz.wisniewski48@wp.pl>
2023-02-21 02:09:17 +0100

    build: added tests to app component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,41 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock("./api/api", () => ({
+  deleteItem: jest.fn(() => Promise.resolve()),
+}));
+
+describe("App", () => {
+  it("renders app and the items list", () => {
+    render(<App />);
+
+    const itemList = screen.getByRole("list", { name: "" });
+    const itemListHeader = screen.getByRole("listitem", { name: "" });
+
+    expect(itemList).toBeInTheDocument();
+    expect(itemListHeader).toBeInTheDocument();
+  });
+
+  it("displays the input when the button is clicked", () => {
+    render(<App />);
+
+    const addButton = screen.getByRole("button", { name: "add" });
+    fireEvent.click(addButton);
+
+    const inputName = screen.getByPlaceholderText("Podaj kanał");
+    expect(inputName).toBeInTheDocument();
+  });
+
+  test("displays error message when data fetching fails", async () => {
+    const errorMessage = "Nie udało się pobrać danych";
+    jest.spyOn(console, "error").mockImplementation(() => {});
+    jest
+      .spyOn(global, "fetch")
+      .mockImplementationOnce(() => Promise.reject(new Error(errorMessage)));
+
+    render(<App />);
+    const error = await screen.findByText(errorMessage);
+    expect(error).toBeInTheDocument();
+    jest.restoreAllMocks();
+  });
 });
